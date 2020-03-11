d67250210b7c8e174b79e3d625f690cfb08be341
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-03-11 19:19:53 -0400

    passed challenge temperatureCtoF

--- index.js
+++ index.js
@@ -54,8 +54,10 @@ function sayGoodbye(/* code here */name) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
+function temperatureCtoF(/* code here */tempC) {
   /* code here */
+  const tempF = ((tempC * 9) / 5) + 32;
+  return Math.round(tempF);
 }
 
 /**
